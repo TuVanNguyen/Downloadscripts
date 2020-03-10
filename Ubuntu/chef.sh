3421a95aa4019ba6b46cd6c5cbc197a4119a6195
Tu Van Nguyen <tuvannguyen33@gmail.com>
2020-03-10 17:02:53 -0500

    Create chef.sh

---
+++ Ubuntu/chef.sh
@@ -0,0 +1,12 @@
+# Ubuntu 18.04
+# Check out here for latest version URL: https://downloads.chef.io/chef-server
+
+wget https://packages.chef.io/files/stable/chef-server/13.1.13/ubuntu/18.04/chef-server-core_13.1.13-1_amd64.deb -O chef-server-core_13.1.13-1_amd64.deb
+
+sudo dpkg -i chef-server-core_13.1.13-1_amd64.deb
+
+sudo chef-server-ctl reconfigure
+
+sudo curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+chef-client -v
