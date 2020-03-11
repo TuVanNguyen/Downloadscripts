9462cdaa0fac7bfe5618688f92b2d294700049f9
Tu Van Nguyen <tuvannguyen33@gmail.com>
2020-03-11 10:38:28 -0500

    Update ansible.sh

--- Ubuntu/ansible.sh
+++ Ubuntu/ansible.sh
@@ -2,6 +2,7 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
+mkdir .ssh
 ssh-keygen -t rsa
 cat .ssh/id_rsa.pub >> .ssh/authorized_keys
 ssh localhost “date” 
